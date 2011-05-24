ded8d0936fac1767c57249905ebf5d8c1660f8f4
Lars Strojny <lars@strojny.net>
2011-05-25 01:41:48 +0200

    Fixing userspace test bootstrap

--- tests/Functional/bootstrap-userspace.php
+++ tests/Functional/bootstrap-userspace.php
@@ -3,4 +3,5 @@ if (extension_loaded('functional')) {
     die("Extension 'functional' loaded. Cannot run userspace implementation\n");
 }
 
+include __DIR__ . '/AbstractTestCase.php';
 include __DIR__ . '/../../src/Functional/_import.php';
