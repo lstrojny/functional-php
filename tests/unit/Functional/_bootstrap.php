9d6967e68e340f4ccfc154497db7172b55de4a66
Lars Strojny <lars@strojny.net>
2011-06-10 22:26:08 +0200

    Restructured tests

--- tests/unit/Functional/_bootstrap.php
+++ tests/unit/Functional/_bootstrap.php
@@ -22,8 +22,8 @@
  */
 
 /** Conditionally includes native implementation */
-require_once __DIR__ . '/../../src/Functional/_import.php';
+require_once __DIR__ . '/../../../src/Functional/_import.php';
 /* @var Functional\Exceptions\InvalidArgumentException */
-require_once __DIR__ . '/../../src/Functional/Exceptions/InvalidArgumentException.php';
+require_once __DIR__ . '/../../../src/Functional/Exceptions/InvalidArgumentException.php';
 /* @var Functional\AbstractTestCase */
 require_once __DIR__ . '/AbstractTestCase.php';
