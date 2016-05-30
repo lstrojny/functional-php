c2642125671eddace5b660e8208eb18dc5344668
Lars Strojny <lars@strojny.net>
2016-05-30 13:38:42 +0200

    Welcome 2016

--- src/Functional/Unique.php
+++ src/Functional/Unique.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Copyright (C) 2011-2015 by Max Beutel <me@maxbeutel.de>, Lars Strojny <lstrojny@php.net>
+* Copyright (C) 2011-2016 by Max Beutel <me@maxbeutel.de>, Lars Strojny <lstrojny@php.net>
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
