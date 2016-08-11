c150b5bf7405eab807f886a4891d1504f5d8520c
Jonas Schürmann <jonasschuermann@aol.de>
2016-08-11 15:11:43 +0200

    Implement, test and document reindex

---
+++ tests/Functional/ReindexTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright (C) 2011-2016 by Lars Strojny <lstrojny@php.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace Functional\Tests;
+
+use ArrayIterator;
+use Functional\Exceptions\InvalidArgumentException;
+use function Functional\reindex;
+
+class ReindexTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->list = ['value', 'value'];
+        $this->listIterator = new ArrayIterator($this->list);
+        $this->hash = ['k1' => 'val1', 'k2' => 'val2'];
+        $this->hashIterator = new ArrayIterator($this->hash);
+    }
+
+    public function test()
+    {
+        $fn = function($v, $k, $collection) {
+            InvalidArgumentException::assertCollection($collection, __FUNCTION__, 3);
+            return $k . $v;
+        };
+        $this->assertSame(['0value' => 'value', '1value' => 'value'], reindex($this->list, $fn));
+        $this->assertSame(['0value' => 'value', '1value' => 'value'], reindex($this->listIterator, $fn));
+        $this->assertSame(['k1val1' => 'val1', 'k2val2' => 'val2'], reindex($this->hash, $fn));
+        $this->assertSame(['k1val1' => 'val1', 'k2val2' => 'val2'], reindex($this->hashIterator, $fn));
+    }
+
+    public function testDuplicateKeys()
+    {
+        $fn = function ($v, $k, $collection) {
+            InvalidArgumentException::assertCollection($collection, __FUNCTION__, 3);
+            return $k[0];
+        };
+        $this->assertSame(['k' => 'val2'], reindex($this->hash, $fn));
+    }
+
+    public function testExceptionIsThrownInArray()
+    {
+        $this->setExpectedException('DomainException', 'Callback exception');
+        reindex($this->list, [$this, 'exception']);
+    }
+
+    public function testExceptionIsThrownInHash()
+    {
+        $this->setExpectedException('DomainException', 'Callback exception');
+        reindex($this->hash, [$this, 'exception']);
+    }
+
+    public function testExceptionIsThrownInIterator()
+    {
+        $this->setExpectedException('DomainException', 'Callback exception');
+        reindex($this->listIterator, [$this, 'exception']);
+    }
+
+    public function testExceptionIsThrownInHashIterator()
+    {
+        $this->setExpectedException('DomainException', 'Callback exception');
+        reindex($this->hashIterator, [$this, 'exception']);
+    }
+
+    public function testPassNoCollection()
+    {
+        $this->expectArgumentError('Functional\reindex() expects parameter 1 to be array or instance of Traversable');
+        reindex('invalidCollection', 'strlen');
+    }
+
+    public function testPassNonCallable()
+    {
+        $this->expectArgumentError('Argument 2 passed to Functional\reindex() must be callable');
+        reindex($this->list, 'undefinedFunction');
+    }
+}
