3bbddaf0d9154cd1a88398e33cfef6e3b7d60523
Lars Strojny <lars@strojny.net>
2016-10-31 23:15:26 +0100

    Add suppress_error() function

---
+++ src/Functional/SuppressError.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright (C) 2011-2016 by Lars Strojny <lstrojny@php.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace Functional;
+
+use ErrorException;
+use function Functional\const_function;
+
+/**
+ * Takes a function and returns a new function that wraps the callback and suppresses the PHP error
+ *
+ * @param callable $callback
+ * @throws ErrorException Throws exception if PHP error happened
+ * @return mixed
+ */
+function suppress_error(callable $callback)
+{
+    return function (...$arguments) use ($callback) {
+        try {
+            set_error_handler(const_function(null));
+
+            return $callback(...$arguments);
+        } finally {
+            restore_error_handler();
+        }
+    };
+}
