9888bc98449335ac4d87772db01a704b6e223e49
Phan An <me@phanan.net>
2017-12-20 11:24:23 +0100

    Add tap() (#152)
    * add tap()
    
    * Fix following feedback

---
+++ tests/Functional/TapTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (C) 2011-2017 by Lars Strojny <lstrojny@php.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace Functional\Tests;
+
+use function Functional\tap;
+
+class TapTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testPassNonCallable()
+    {
+        $this->expectArgumentError('Argument 2 passed to Functional\tap() must be callable');
+        tap('foo', 'undefinedFunction');
+    }
+
+    public function testTap()
+    {
+        $input = new \stdClass();
+        $input->property = 'foo';
+
+        $output = tap($input, function ($o) {
+            $o->property = 'bar';
+        });
+
+        self::assertSame($input, $output);
+        self::assertSame('bar', $input->property);
+    }
+}
