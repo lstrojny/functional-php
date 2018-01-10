94c0197992952626facb94815d07f88c39622e46
Giovanni Ornaghi <sphaso@gmail.com>
2018-01-10 11:24:11 +0100

    init function (#154)
    but_last() function to return all elements but the last

---
+++ tests/Functional/ButLastTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (C) 2011-2017 by Lars Strojny <lstrojny@php.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the 'Software'), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace Functional\Tests;
+
+use ArrayIterator;
+use Functional\Exceptions\InvalidArgumentException;
+use function Functional\but_last;
+
+class ButLastTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->list = [1, 2, 3, 4];
+        $this->listIterator = new ArrayIterator($this->list);
+    }
+
+    public function test()
+    {
+        $this->assertSame([0 => 1, 1 => 2, 2 => 3], but_last($this->list));
+        $this->assertSame([0 => 1, 1 => 2, 2 => 3], but_last($this->listIterator));
+        $this->assertSame([], but_last([]));
+    }
+
+    public function testPassNoCollection()
+    {
+        $this->expectArgumentError('Functional\but_last() expects parameter 1 to be array or instance of Traversable');
+        but_last('invalidCollection');
+    }
+}
