ad864b27aa64462da9d79a73241fc7d7ff6a2fe4
Viktor Szépe <viktor@szepe.net>
2019-12-02 11:09:47 +0100

    Add file comments (#202)

--- tests/Functional/FunctionalTest.php
+++ tests/Functional/FunctionalTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @package   Functional-php
+ * @author    Lars Strojny <lstrojny@php.net>
+ * @copyright 2011-2017 Lars Strojny
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @link      https://github.com/lstrojny/functional-php
+ */
+
 namespace Functional\Tests;
 
 use Functional\Functional;
