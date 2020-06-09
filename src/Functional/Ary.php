f9659458c46807892c498d2a5abee4612e50306b
Hugo Sales <hugo@fc.up.pt>
2020-06-09 14:16:59 +0000

    Add `ary` function which ignores arguments over the supplied number (#215)
    * Add `ary`, a function which ignores arguments over the supplied number
    
    Added function which defines a function of an arbitry number of arguments and
    calls the provided callable with only the given number of arguments
    
    As suggested by @lstrojny, using the name `ary`, like in https://lodash.com/docs/4.17.15#ary
    
    * Added test for `ary` function
    
    * Fixed errors spit by vendor/bin/phpcs
    
    * Fix implementation and tests

---
+++ src/Functional/Ary.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package   Functional-php
+ * @author    Hugo Sales <hugo@fc.up.pt>
+ * @copyright 2020 Hugo Sales
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @link      https://github.com/lstrojny/functional-php
+ */
+
+namespace Functional;
+
+use Functional\Exceptions\InvalidArgumentException;
+use Traversable;
+
+/**
+ * Call $func with only abs($count) arguments, taken either from the
+ * left or right depending on the sign
+ */
+function ary(callable $func, int $count): callable
+{
+    InvalidArgumentException::assertNonZeroInteger($count, __FUNCTION__);
+
+    return function (...$args) use ($func, $count) {
+        if ($count > 0) {
+            return $func(...take_left($args, $count));
+        } else if ($count < 0) {
+            return $func(...take_right($args, -$count));
+        }
+    };
+}
--- src/Functional/Exceptions/InvalidArgumentException.php
+++ src/Functional/Exceptions/InvalidArgumentException.php
@@ -280,6 +280,13 @@ class InvalidArgumentException extends \InvalidArgumentException
         }
     }
 
+    public static function assertNonZeroInteger($value, $callee)
+    {
+        if (!\is_int($value) || $value == 0) {
+            throw new static(\sprintf('%s expected parameter %d to be non-zero', $callee, $value));
+        }
+    }
+
     private static function getType($value)
     {
         return \is_object($value) ? \get_class($value) : \gettype($value);
