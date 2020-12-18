b8f8ba41889f3835a261105ba70c61b63dec8319
Adrian Panicek <apanicek@pixelfederation.com>
2020-12-18 19:17:26 +0100

    Adding key_sort functionality

--- src/Functional/Functional.php
+++ src/Functional/Functional.php
@@ -224,6 +224,11 @@ final class Functional
     const invoker = '\Functional\invoker';
 
     /**
+     * @see \Functional\key_sort
+     */
+    const key_sort = '\Functional\key_sort';
+
+    /**
      * @see \Functional\last
      */
     const last = '\Functional\last';
